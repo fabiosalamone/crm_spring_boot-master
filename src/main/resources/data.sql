e1882e1e74027b6c57918624e6e291e75eebbcf7
Fabio Salamone <59093291+fabiosalamone@users.noreply.github.com>
2022-07-10 09:41:34 +0200

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,2 @@
 insert into customer (first_name, last_name, email_address, address, city, country, phone_number) values ('Fabio', 'Salamone', 'fabios159@gmail.com', 'Via Roma 5', 'Milano', 'Italia', '+39 3402523177');
+insert into quote (description, price, customer_id) values ('Esempio descrizione di un preventivo', 500, 1);
